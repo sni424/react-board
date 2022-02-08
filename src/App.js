26548ff37796facd831b962bc24eed538824b45b
Your Name <you@example.com>
2022-02-09 05:46:37 +0900

    today

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@
 import React, { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Leftheader from './view/Leftheader';
+import Leftheader from "./view/Leftheader";
 import Community from './view/Community';
 import Write from './view/Write';
-import Data from "./db/data.json"
+import Readview from './view/Readview';
+import Setview from './view/Setview';
+import useFetch from './hooks/useFetch';
 
 function App() {
 
@@ -15,23 +17,34 @@ function App() {
     setCommunity(!community);
   };
 
+  let [Data, setData] = useState([]);
   useEffect(() => {
-    console.log(Data.titles);
-  });
-
+    const reloadProfile = async () => {
+      try {
+        let profileData = await fetch("http://localhost:3001/titles");
+        let newData = await profileData.json();
+        return setData(newData);
+      } catch (error) {
+        console.log(error)
+      }
+    };
+    reloadProfile();
+  }, ["http://localhost:3001/titles"]);
 
   return (
     <div className="App">
       <Routes>
-        <Route path="/*" element={<Leftheader showCommnutiy={showCommnutiy}></Leftheader>}>
+        <Route path="/*" element={<Leftheader Data={Data} showCommnutiy={showCommnutiy}></Leftheader>}>
           <Route path='/*' element={community === true
-            ? <Community></Community>
+            ? <Community ></Community>
             : null
           } />
 
         </Route>
-        <Route path="/write" element={<Write></Write>}>
+        <Route path="/write" element={<Write Data={Data}></Write>}>
         </Route>
+        <Route path="/:link_id" element={<Readview Data={Data}></Readview>}></Route>
+        <Route path="/setview:link_id" element={<Setview Data={Data}></Setview>}></Route>
       </Routes>
     </div >
   );
--- src/view/Leftheader.js
+++ src/view/Leftheader.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*eslint-disable */
 import React, { useState } from "react";
 import Logo from "../img/okjsp_logo.png"
@@ -31,42 +30,9 @@ function Leftheader(props) {
                 </div>
             </div>
             <Outlet></Outlet>
-            <MainCommunity />
+            <MainCommunity Data={props.Data} />
         </div>
     );
 };
 
-=======
-/*eslint-disable */
-import React, { useState } from "react";
-import Logo from "../img/okjsp_logo.png"
-import "../css/Leftheader.css"
-
-function Leftheader(props) {
-
-    return (
-        <div className='leftbar-ca leftbar'>
-            <div className='logo'><a href='/'><img src={Logo} alt='logo'></img></a></div>
-            <div className='search'><a href='#'><i className="fa fa-search"></i></a></div>
-            <div className='second-icon'>
-                <div className='login-icon'><a href='#'><i className="fa fa-sign-in"></i></a></div>
-                <div className='signin-icon'><a href='#'><i className="fa fa-user"></i></a></div>
-            </div>
-            <div>
-                <ul className='ulfirst-tag'>
-                    <li><a><i className="nav-icon fa fa-database"></i></a></li>
-                    <li><a><i className="nav-icon nav-icon fa fa-code"></i></a></li>
-                    <li><a onClick={props.showCommnutiy}><i className="nav-icon fa fa-comments"></i></a></li>
-                    <li><a><i className="nav-icon fa fa-quote-left"></i></a></li>
-                    <li><a><i className="nav-icon fa fa-group"></i></a></li>
-                </ul>
-            </div>
-            <div className='github-logo'>
-                <a href='#'><i className="fa fa-github"></i></a>
-            </div>
-        </div>
-    );
-};
-
->>>>>>> master
 export default Leftheader;
